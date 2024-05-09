211143f8ef9b9238ddfa08daba711926f091beee
Il tuo nome <la_tua_email@example.com>
2024-05-09 15:02:02 +0200

    set up

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const userName = prompt("Come ti chiami?");
+// console.log("Ciao " + userName);
+// console.log
+
+// // let number = 100
+// // console.log (number);
+
+// // const pi = 3.14
+// // console.log (pi);
+
+// // pari o dispari: chiedere all'utente/ssa se vuole pari o dispari e un numero intero compreso tra 1 e 9. Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer. Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
+
+// // let choise = prompt ("Odd or even?")
+// // let number = prompt ("Select a number")
+
+// // let pcnum = Math.floor(Math.random () * 10);
+
+// // console.log(pcnum)
+
+
+// let element = button
+
+// document.addEventListener("click", function(){
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
